0b46001c9f9f44548f2a3ca036dece3ae291a420
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-07-15 14:58:54 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,9 +1,31 @@
+
+
 const form = document.querySelector(".login-form")
 
 form.addEventListener("submit", dataUser)
 
 function dataUser(e) {
     e.preventDefault();
-    console.dir(form);
+    const email = form.elements.email.value;
+    const pass = form.elements.password.value;
+    if (email === '' || pass === '') {
+        alert('Заповніть пусті поля!')
+    }
+    else {
+        const saveMailPass = {
+            email: [],
+            password: [],
+        }
+        saveMailPass.email.push(email);
+        saveMailPass.password.push(pass);
+        form.reset();
+        console.log(saveMailPass);
+    }
+    // console.dir(form);
+    // console.log(email);
+    // console.log(pass);
+
+
+
 }
 
