fa02b5059d763974ac114b7023f8a82f9a7339f5
Zhihare <128489441+Zhihare@users.noreply.github.com>
2023-07-24 13:54:32 +0300

    24.07

--- js/task-10.js
+++ js/task-10.js
@@ -8,15 +8,25 @@ const controlBut = document.getElementById("controls");
 const createEl = document.getElementById("boxes");
 const dataCreate = document.querySelector("[data-create]");
 const dataDestroy = document.querySelector("[data-destroy]")
+const inputNumbers = document.querySelector(".numDiv");
 
 dataCreate.addEventListener("click", createBoxes());
 dataDestroy.addEventListener("click", destroyBoxes());
 
 function createBoxes(amount) {
-  console.dir(dataCreate)
-  for (let i = 0; i < amount; i += 1) {
-    const addEll = createEl.createElement("div")
+  // console.dir(dataCreate);
+  // console.dir(controlBut);
+
+  if (inputNumbers.value === "" || inputNumbers.value > 100) {
+    return 'Ввведіть кількість';
   }
+  // else {
+  //   let inputNum = inputNumbers.value;
+  //   console.dir(inputNum)
+  //   for (let i = 0; i < amount; i += 1) {
+  //     const addEll = createEl.createElement("div")
+  //   }
+  // }
 }
 
 function destroyBoxes() {
